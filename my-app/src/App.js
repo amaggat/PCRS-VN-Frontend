64aca66019a4ee969996363fed50102f3f851309
manhdung20112000 <manhdung20112000@gmail.com>
2020-12-10 20:26:08 +0700

    basic structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Page from "./Pages/Page"
+import {BrowserRouter as Router} from "react-route-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Page/>
+      </Router>
     </div>
   );
 }
---
+++ my-app/src/Pages/Page.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+
+import Home from "./Home/Home"
+import About from "./About/About"
+import Help from "./Help/Help"
+
+import List from "./List/List"
+import Build from "./Build/Build"
+import Products from './Product/Product'
+
+import CPU from "./Product/Part/CPU"
+import MotherBoard from './Product/Part/MotherBoard'
+import Memory from './Product/Part/Memory'
+import Storage from './Product/Part/Storage'
+import VideoCard from './Product/Part/VideoCard'
+import Power from './Product/Part/Power'
+import Case from './Product/Part/Case'
+
+import './Page.css'
+
+const NotFound = ()=> <h1>404</h1>
+
+class Page extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <div className="page-body">
+                    <Switch>
+                        {/* Basic Page */}
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                        <Route path="/help" component={Help} />
+                        {/* Functional Page */}
+                        <Route path="/list" component={List} />
+                        <Route path="/builds" component={Build} />
+                        <Route exact path="/products" component={Products} />
+                        {/* Browse Product subpages */}
+                        <Route path="/products/cpu" component={CPU} />
+                        <Route path="/products/mother-board" component={MotherBoard} />
+                        <Route path="/products/memory" component={Memory} />
+                        <Route path="/products/storage" component={Storage} />
+                        <Route path="/products/video-card" component={VideoCard} />
+                        <Route path="/products/power" component={Power} />
+                        <Route path="/products/case" component={Case} />
+                        {/* 404 */}
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Page;
