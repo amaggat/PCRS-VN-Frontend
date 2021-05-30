36089c3b6c9712abad6ebf5c78f0177206e08a1d
fuzeless <lequangnhat2000@gmail.com>
2021-05-30 19:23:44 +0700

    Add recording component and bugs fixing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,9 @@
 import Page from "./Pages/Page";
-import { Widget, addResponseMessage, addLinkSnippet } from 'react-chat-widget';
+import { useState } from "react";
+import styled from "styled-components";
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+
+import { Widget, addResponseMessage, addLinkSnippet, toggleWidget } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
 import Cookies from 'js-cookie';
 import { ToastContainer } from 'react-toastify';
@@ -8,12 +12,52 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { sendMessage } from './Client/AccountService';
 import './App.css';
 
+const RecordButtonWrapper = styled.div`
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    margin: 0 20px 20px 0;
+    position: fixed;
+    right: 70px;
+    width: 60px;
+    z-index: 99999;
+`;
+
+const OnClickOverlay = styled.div`
+    bottom: 0;
+    width: 60px;
+    height: 60px;
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
+    z-index: 99999;
+    cursor: pointer;
+`
+
+const RecordButton = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #E60001;
+    font-size: x-large;
+    color: white;
+    border: 0;
+    border-radius: 50%;
+    box-shadow: 0 2px 10px 1px #b5b5b5;
+    height: 60px;
+    width: 60px;
+    margin-top: 10px;
+    cursor: pointer;
+`;
 function App() {
+    const [showRecordButton, setShowRecordButton] = useState(false);
+    const [recordState, setRecordState] = useState(null);
     const handleMessage = async (newMessage) => {
         const result = await sendMessage(newMessage);
         if (result.data) {
             addResponseMessage(result.data.response);
-            const productList = result.data.responseList;
+            const productList = result.data.responseList || [];
             productList.forEach(product => {
                 addLinkSnippet({
                     title: product.key || 'No name',
@@ -23,6 +67,34 @@ function App() {
             });
         }
     }
+
+    const handleRecordClick = () => {
+        if (recordState === RecordState.START) setRecordState(RecordState.STOP);
+        else setRecordState(RecordState.START);
+    }
+
+    const onRecordStop = (audioData) => {
+        console.log('Record: ', audioData);
+        //TODO: API Pending...
+    }
+
+    const handleChatWidgetClick = () => {
+        setShowRecordButton(!showRecordButton);
+        toggleWidget();
+    }
+
+    const recordButton = (
+        <RecordButtonWrapper>
+            <RecordButton onClick={handleRecordClick}>
+                {
+                    recordState === RecordState.STOP || recordState === null
+                    ? <i class="fas fa-microphone" />
+                    : <i class="fas fa-square" />
+                }
+            </RecordButton>
+        </RecordButtonWrapper>
+    )
+
     return (
         <div className="App">
             <Router>
@@ -39,6 +111,15 @@ function App() {
                     pauseOnHover
                 />
                 {
+                    showRecordButton
+                        ? recordButton
+                        : null
+                }
+                {/* Tricky way to insert onClick action to ChatWidget. */}
+                {/* Sorry in advance for any pains this could have caused */}
+                <OnClickOverlay onClick={handleChatWidgetClick} />
+                <AudioReactRecorder state={recordState} onStop={onRecordStop} />
+                {
                     Cookies.get('jwt')
                         ? (
                             <Widget
@@ -46,7 +127,6 @@ function App() {
                                 subtitle=":)"
                                 senderPlaceHolder="Type something..."
                                 handleNewUserMessage={handleMessage}
-                                
                             />
                         )
                         : null
